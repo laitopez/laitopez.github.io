595a4ddd68ac16c678622e6f7703d22f5d5f8a15
laitopez <laitopez13@gmail.com>
2025-05-23 17:45:45 -0400

    init commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "LT",
+	url: "laitopez.github.io",
+	language: "en",
+	description: "LT blog",
+	author: {
+		name: "LT",
+		email: "",
+		url: "laitopez.github.io"
+	}
+}
